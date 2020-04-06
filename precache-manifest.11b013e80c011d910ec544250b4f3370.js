489c59ee8ba7b4e4200e48a4e01b98800ae99688
masterambi <ramzyrash@gmail.com>
2020-04-06 18:23:28 +0700

    Updates

---
+++ precache-manifest.11b013e80c011d910ec544250b4f3370.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "829c454c95fc84a49d4ec904936cde2c",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "0f909cb5d76414d417e2",
+    "url": "/smart-brain/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "29a6e36432d566f0cb49",
+    "url": "/smart-brain/static/css/main.ca0c674c.chunk.css"
+  },
+  {
+    "revision": "0f909cb5d76414d417e2",
+    "url": "/smart-brain/static/js/2.15980fc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-brain/static/js/2.15980fc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29a6e36432d566f0cb49",
+    "url": "/smart-brain/static/js/main.a8ba4318.chunk.js"
+  },
+  {
+    "revision": "8334c4c754da6c8a5e5a",
+    "url": "/smart-brain/static/js/runtime-main.9472589a.js"
+  }
+]);
